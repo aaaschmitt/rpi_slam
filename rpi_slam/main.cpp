f6b3cea1fceabb00d15462ea60be8ae3e35b8058
Travis CI <aschmitt@berkeley.edu>
2016-09-23 00:43:44 -0700

    Initial Commit

---
+++ rpi_slam/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  rpi_slam
+//
+//  Created by Andrew Schmitt on 9/23/16.
+//  Copyright © 2016 Andrew Schmitt. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
