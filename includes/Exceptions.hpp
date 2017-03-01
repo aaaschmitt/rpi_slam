f8f14221b321c955b664ec4b7a4080cf82bd7827
Travis CI <aschmitt@berkeley.edu>
2017-02-28 20:11:58 -0800

    removed the wiring pi files and did some real work on the concurrent queue

---
+++ includes/Exceptions.hpp
@@ -0,0 +1,13 @@
+//
+//  Exceptions.hpp
+//  rpi_slam
+//
+//  Created by Andrew Schmitt on 10/1/16.
+//  Copyright © 2016 Andrew Schmitt. All rights reserved.
+//
+
+#ifndef Exceptions_h
+#define Exceptions_h
+
+
+#endif /* Exceptions_h */
---
+++ src/Exceptions.cpp
@@ -0,0 +1,9 @@
+//
+//  Exceptions.cpp
+//  rpi_slam
+//
+//  Created by Andrew Schmitt on 10/1/16.
+//  Copyright © 2016 Andrew Schmitt. All rights reserved.
+//
+
+#include <stdio.h>
